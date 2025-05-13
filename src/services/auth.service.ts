a6349e03cfecce26ffbd037499a4bf1212fde653
Artem Kuryshko <kuryshkoartem05@gmail.com>
2025-05-13 17:20:31 +0300

    fixed any issue

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,42 +1,42 @@
-import { auth } from "@/auth";
-import axios from "axios";
-import { JWT } from "next-auth/jwt";
+import { auth } from '@/auth'
+import axios from 'axios'
+import { JWT } from 'next-auth/jwt'
 
 export const AuthService = {
-    getToken: async (): Promise<string> => {
-        const session = await auth();
-        return session?.accessToken as string;
-    },
-    refreshToken: async (token: JWT) => {
-        const url = "https://accounts.spotify.com/api/token";
+	getToken: async (): Promise<string> => {
+		const session = await auth()
+		return session?.accessToken as string
+	},
+	refreshToken: async (token: JWT) => {
+		const url = 'https://accounts.spotify.com/api/token'
 
-        const clientId = process.env.AUTH_SPOTIFY_ID as string;
-        const clientSecret = process.env.AUTH_SPOTIFY_SECRET as string;
+		const clientId = process.env.AUTH_SPOTIFY_ID as string
+		const clientSecret = process.env.AUTH_SPOTIFY_SECRET as string
 
-        const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+		const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
 
-        const params = new URLSearchParams();
-        params.append("grant_type", "refresh_token");
-        params.append("refresh_token", token.refreshToken as string);
+		const params = new URLSearchParams()
+		params.append('grant_type', 'refresh_token')
+		params.append('refresh_token', token.refreshToken as string)
 
-        try {
-            const { data } = await axios.post(url, {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": `Basic ${credentials}`,
-                },
-                data: params,
-            });
+		try {
+			const { data } = await axios.post(url, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Authorization': `Basic ${credentials}`
+				},
+				data: params
+			})
 
-            return {
-                ...token,
-                access_token: data.access_token,
-                expires_in: Date.now() + data.expires_in * 1000,
-                refresh_token: data.refresh_token || token.refreshToken, // Keep the old one if not returned
-            };
-        } catch (error: any) {
-            console.error("Error refreshing Spotify token:", error.response?.data || error);
-            throw new Error("Failed to refresh token");
-        }
-    }
-};
+			return {
+				...token,
+				access_token: data.access_token,
+				expires_in: Date.now() + data.expires_in * 1000,
+				refresh_token: data.refresh_token || token.refreshToken // Keep the old one if not returned
+			}
+		} catch (error: Error) {
+			console.error('Error refreshing Spotify token:', error.response?.data || error)
+			throw new Error('Failed to refresh token')
+		}
+	}
+}
